cac341c516d425e901adbeac182f262eaa6eaa5c
Anthony Sekatski <reterius@gmail.com>
2014-01-17 01:02:27 +0400

    Add backward method: ToUUID

--- url62/url62.go
+++ url62/url62.go
@@ -2,22 +2,44 @@ package url62
 
 import (
 	//"github.com/nu7hatch/gouuid"
+	"errors"
+	"fmt"
 	"math/big"
 	"strings"
 )
 
 const base62alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func FromUUID(shex string) string {
+func FromUUID(shex string) (string, error) {
 	u := strings.Replace(shex, "-", "", 4)
 
-	return convertUp(u, base62alphabet)
+	converted, err := convertUp(u, base62alphabet)
+	if err != nil {
+		return "", err
+	}
+
+	return converted, nil
+}
+
+func ToUUID(s string) (string, error) {
+	//.insert(8, '-').insert(13, '-').insert(18, '-').insert(23, '-')
+	u, err := convertDown(s, base62alphabet)
+	if err != nil {
+		return "", err
+	}
+
+	u = insert(u, "-", 8)
+	u = insert(u, "-", 13)
+	u = insert(u, "-", 18)
+	u = insert(u, "-", 23)
+
+	return u, nil
 }
 
 // ConvertUp converts a hexadecimal UUID string to a base alphabet greater than
 // 16. It is used here to compress a 32 character UUID down to 23 URL friendly
 // characters.
-func convertUp(oldNumber string, baseAlphabet string) string {
+func convertUp(oldNumber string, baseAlphabet string) (string, error) {
 	n := big.NewInt(0)
 	n.SetString(oldNumber, 16)
 
@@ -31,5 +53,35 @@ func convertUp(oldNumber string, baseAlphabet string) string {
 		_, r := n.DivMod(n, base, big.NewInt(0))
 		newNumber[i] = baseAlphabet[r.Int64()]
 	}
-	return string(newNumber[i:])
+	return string(newNumber[i:]), nil
+}
+
+func convertDown(oldNumber string, baseAlphabet string) (string, error) {
+	// Return if oldNumber is less or bigger than 22
+	if len(oldNumber) != 22 {
+		return "", errors.New("Not valid base62 string")
+	}
+
+	n := big.NewInt(0)
+
+	s := strings.Split(oldNumber, "")
+
+	var i int
+
+	for _, v := range s {
+		i = strings.Index(base62alphabet, v)
+
+		if i == -1 {
+			return "", errors.New("Not valid base62 string. String does not contain a right Base62 alphabet")
+		}
+
+		n.Mul(n, big.NewInt(62))
+		n.Add(n, big.NewInt(int64(i)))
+	}
+
+	return fmt.Sprintf("%02x", n), nil
+}
+
+func insert(s string, sep string, i int) string {
+	return s[0:i] + sep + s[i:len(s)]
 }
--- url62/url62_test.go
+++ url62/url62_test.go
@@ -4,10 +4,26 @@ import (
 	"testing"
 )
 
-func TestFromUUID(t *testing.T) {
-	u := FromUUID("6ba7b814-9dad-11d1-80b4-00c04fd430c8")
+func Test_FromUUID(t *testing.T) {
+	u, _ := FromUUID("6ba7b814-9dad-11d1-80b4-00c04fd430c8")
 
 	if u != "3h8Pgh03y0pa2W6ltuUfZ6" {
+		t.Error("Not valid")
+	}
+}
+
+func Test_ToUUID(t *testing.T) {
+	u, _ := ToUUID("3h8Pgh03y0pa2W6ltuUfZ6")
+
+	if u != "6ba7b814-9dad-11d1-80b4-00c04fd430c8" {
+		t.Error("Not valid")
+	}
+}
+
+func Test_Wrong_ToUUID(t *testing.T) {
+	_, err := ToUUID("3h8Pgh0")
+
+	if err == nil {
 		t.Error("Not valid")
 	}
 }
